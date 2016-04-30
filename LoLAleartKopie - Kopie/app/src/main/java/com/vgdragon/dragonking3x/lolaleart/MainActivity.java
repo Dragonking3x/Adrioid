776386bd305bdaf49afdd114de9b8281140e0371
Dragonking3x <Patrick__Promitzer@hotmail.com>
2016-04-30 03:38:29 +0200

    Threat fixing

--- LoLAleartKopie - Kopie/app/src/main/java/com/vgdragon/dragonking3x/lolaleart/MainActivity.java
+++ LoLAleartKopie - Kopie/app/src/main/java/com/vgdragon/dragonking3x/lolaleart/MainActivity.java
@@ -1,8 +1,16 @@
 package com.vgdragon.dragonking3x.lolaleart;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -38,15 +46,7 @@ public class MainActivity extends AppCompatActivity {
     String setConnectTxt = "";
     Context context = this;
 
-    public Thread getT() {
-        return t;
-    }
 
-    public void setT(Thread t) {
-        this.t = t;
-    }
-
-    private Thread t;
 
     public boolean isConnectButton() {
         return connectButton;
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean uiStart = true;
 
+    //Ringtone sound;
 
 
 
@@ -69,11 +70,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //try{
+        //    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        //    this.sound = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        //} catch (Exception e){
+//
+        //}
+
+
+
 
         final Button connect = (Button) findViewById(R.id.connectButton);
         ipAdress = (EditText) findViewById(R.id.ipTxtField);
         gameInfo = (TextView) findViewById(R.id.gameInfoTxt);
         connectTxt = (TextView) findViewById(R.id.connectTxt);
+
 
 
 
@@ -90,8 +101,8 @@ public class MainActivity extends AppCompatActivity {
         lol.setMa(ma);
         lol.setContext(context);
         lol.setConnectTxt(connectTxt);
-        final Thread t = new Thread(lol);
-        setT(t);
+
+
 
 
         final Thread txtUpdate = new Thread() {
@@ -153,18 +164,20 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     lol.setConnecting(1);
                     connect.setText("Disconnect");
-                    setConnectButton(true);
+                    lol.setIAlive(1);
+                    final Thread t = new Thread(lol);
+                    t.start();
                 }
 
 
-                if (!getT().isAlive()) {
-                    if (lol.getIAlive() == 0) {
-                        lol.setIAlive(1);
-                        t.start();
-                    }
-
-
-                }
+                //if (!getT().isAlive()) {
+                //    if (lol.getIAlive() == 0) {
+                //        lol.setIAlive(1);
+                //        t.start();
+                //    }
+//
+//
+                //}
 
 
 
@@ -186,10 +199,66 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void playSound() {
-        sound = MediaPlayer.create(this, R.raw.sound);
-        sound.start();
+        //sound = MediaPlayer.create(this, R.raw.sound);
+        //sound.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+
+        //sound.start();
+
+
+
+
+        //this.sound.play();
+        setNotifiaktionsBar();
+    }
+
+    public void setNotifiaktionsBar(){
+
+        Intent notificationTintent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationTintent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setContentTitle("LoL")
+                .setContentText("Game Start")
+                .setContentIntent(pendingIntent);
+
+        Notification notification = mBuilder.build();
+
+        notification.defaults |= Notification.DEFAULT_VIBRATE;
+        notification.defaults |= Notification.DEFAULT_SOUND;
+
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notification.tickerText = "LoL \n Game Start"; //edit for custam text
+
+        /////
+
+        //Intent notificationIntent = new Intent(this, MainActivity.class);
+        //PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+        //        notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+        //NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+//
+        //        .setContentTitle("my title")
+        //        .setContentText("my content")
+        //        .setContentIntent(pendingIntent);
+        //Notification notification = mBuilder.build();
+        //// default phone settings for notifications
+        //notification.defaults |= Notification.DEFAULT_VIBRATE;
+        //notification.defaults |= Notification.DEFAULT_SOUND;
+//
+        //// cancel notification after click
+        //notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        //// show scrolling text on status bar when notification arrives
+        //notification.tickerText = "\n";
+
+        // notifiy the notification using NotificationManager
+
 
     }
+
+
+
 
 
     protected void setGameinfo(String text) {
